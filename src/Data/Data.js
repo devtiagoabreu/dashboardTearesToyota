b74b9dfdb22e27f720f274b2459715a65213797d
Tiago Abreu <devtiagoabreu@gmail.com>
2024-04-28 13:44:19 -0300

    Sidebar

---
+++ src/Data/Data.js
@@ -0,0 +1,35 @@
+//Sidebar imports
+
+import { 
+  UilEstate,
+  UilUsersAlt,
+  UilClipboardAlt,
+  UilPackage,
+  UilChart,
+} from '@iconscout/react-unicons';
+
+export const SidebarData = [
+  {
+    icon: UilEstate,
+    heading: "Dashboard",    
+  },
+  {
+    icon: UilClipboardAlt,
+    heading: "Ordens",    
+  },
+  {
+    icon: UilUsersAlt,
+    heading: "Customers",    
+  },
+  {
+    icon: UilPackage,
+    heading: "Products",    
+  },
+  {
+    icon: UilChart,
+    heading: "Analytics",    
+  },
+  
+];
+ 
+
